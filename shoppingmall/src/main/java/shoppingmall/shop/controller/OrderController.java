f234c451a7f2fe93c59fd639f25ea1258157f6c3
ABCchocolate <qrkdbtls@gmail.com>
2025-08-01 15:20:21 +0900

    Rename Shoppingmall to shoppingmall (case fix)

---
+++ shoppingmall/src/main/java/shoppingmall/shop/controller/OrderController.java
@@ -0,0 +1,44 @@
+package shoppingmall.shop.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import shoppingmall.domain.Cart;
+import shoppingmall.model.order.CartService;
+
+@Controller
+public class OrderController {
+	
+	@Autowired
+	private CartService cartService;
+
+
+	
+	@PostMapping("/product/order")
+	@ResponseBody
+	public Map<String, String> goToOrder(@RequestBody List<Integer> cartIds, HttpSession session) {
+	    List<Cart> orderItems = cartService.getCartItemsByIds(cartIds);
+	    session.setAttribute("orderItems", orderItems);
+	    Map<String, String> map = new HashMap<>();
+	    map.put("url", "/shop/product/order"); // 실제 order.jsp 경로
+	    return map;
+	}
+	
+	@GetMapping("/product/order")
+	public String showOrderPage(HttpSession session, Model model) {
+	    List<Cart> orderItems = (List<Cart>) session.getAttribute("orderItems");
+	    model.addAttribute("orderItems", orderItems);
+	    return "shop/product/order"; 
+	}
+}
