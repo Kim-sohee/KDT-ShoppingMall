580cd1cf52062196d29d003c0055549c5fc86de3
ABCchocolate <qrkdbtls@gmail.com>
2025-08-01 11:04:13 +0900

    cart 내 삭제 mapper 구현

--- shoppingmall/src/main/java/shoppingmall/model/order/CartDAO.java
+++ shoppingmall/src/main/java/shoppingmall/model/order/CartDAO.java
@@ -9,4 +9,10 @@ public interface CartDAO {
 	
 	//고객에게 해당하는 장바구니 목록 불러오기
 	public List<Cart> selectByMemeber(int product_id);
+	
+	public void deleteCartItem(int cartId);
+	public Cart select(int cart_id);
+	public void updateQuantity(Cart cart);
+	public void deleteSelectedItems(List cartIds);
+	public List<Cart> getCartItemsByIds(List<Integer> ids);
 }
